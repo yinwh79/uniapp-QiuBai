c28fd50fd7b43de92685d0607bde2bb654c715c6
woodensRangerWZJ <13029768866@163.com>
2019-09-19 16:34:12 +0800

    弹出提示框

--- pages/AddInput/AddInput.vue
+++ pages/AddInput/AddInput.vue
@@ -24,21 +24,42 @@
         <upload-img
           @upload = "uploadImg"
         ></upload-img>
+        <!-- 弹出公告 -->
+        <uni-popup 
+          :show="showPopup"
+          mode="fixed"
+          position="middle"
+          @hidePopup="hidePopup"
+        >
+          <view class="gonggao">
+            <view class="pop-img">
+              <image src="../../static/common/addinput.png" mode="widthFix"></image>
+              </view>
+              <view>1.涉及黄色，政治，广告及骚扰信息</view>
+              <view>2.涉及黄色，政治，广告及骚扰信息</view>
+              <view>3.涉及黄色，政治，广告及骚扰信息</view>
+              <view>4.涉及黄色，政治，广告及骚扰信息</view>
+              <button type="default">朕知道了</button>
+          </view>
+        </uni-popup>
 	</view>
 </template>
 
 <script>
   import UniNavBar from "../../components/uni-nav-bar/uni-nav-bar.vue"
   import UploadImg from "../../components/common/UploadImg.vue"
+  import UniPopup from '../../components/uni-popup/uni-popup.vue'
   
   let dynamicTypeArr =  ['所有人可见', '仅自己可见']
 	export default {
     components:{
       UniNavBar,
-      UploadImg
+      UploadImg,
+      UniPopup
     },
 		data() {
 			return {
+        showPopup: true,
 				dynamicType: '所有人可见',
         dynamicContent: "",
         imgList: []
@@ -67,6 +88,10 @@
       uploadImg(arr){
         this.imgList = arr
         console.log(this.imgList);
+      },
+      // 隐藏弹窗
+      hidePopup(){
+        this.showPopup = false
       }
     }   
 	}
@@ -80,4 +105,17 @@
     justify-content: center;
   .uni-textarea
     border: 1upx solid #eee;
+  // 公告
+    .gonggao
+      width: 450upx;
+      .pop-img
+        display: flex;
+        justify-content: center;
+        margin-bottom: 20upx;
+      image
+        width: 85%;
+      button
+        margin-top: 20upx;
+        color #171606
+        background-color: #ffe934;
 </style>
