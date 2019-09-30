50b2f1fd38cee2fb254d2640d9d431a5e4cbade4
woodensRangerWZJ <13029768866@163.com>
2019-09-30 19:11:24 +0800

    登录页面骨架完成

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -2,27 +2,159 @@
 	<view>
     <!-- 状态栏 -->
     <uni-status-bar></uni-status-bar>
-		登录
+    <!-- 返回按钮 -->
+    <view 
+      class="icon iconfont icon-guanbi"
+      @tap="onBack"
+     ></view> 
+    <!-- 背景图片 -->
+    <image
+      class="login-head"
+      src="../../static/common/loginhead.png" 
+      mode="widthFix" 
+      lazy-load></image> 
+    <!-- 登录区域 -->
+    <!-- 账号密码登录 -->
+    <view class="wrapper">      
+      <input
+        type="password" 
+        class="uni-input common-input"
+        placeholder="昵称/手机号/邮箱"/>
+      <view class="login-password-wrapper">
+        <input
+          type="password" 
+          class="uni-input common-input forgot-input" 
+          placeholder="请输入密码"/>
+          <view class="forgot-password">忘记密码?</view>
+      </view>
+      <!-- 手机号登录 -->
+      <view class="wrapper">
+          <view class="login-password-wrapper">
+            <view class="phone">+86</view>
+            <input
+              type="password" 
+              class="uni-input common-input phone-input"
+              placeholder="手机号"/>
+          </view>
+          <view class="login-password-wrapper">
+            <input
+              type="password" 
+              class="uni-input common-input forgot-input" 
+              placeholder="请输入验证码"/>
+              <view class="get-yzm">获取验证码</view>
+          </view>
+      </view>
+      <button 
+        type="primary" 
+        class="user-set-btn"
+       :disabled="disabled"
+       :loading="loading"
+      >登录</button>
+    </view>
+    <!-- 登录状态切换 -->
+    <view class="login-type flex-c-c">
+      验证码登录
+      <view class="icon iconfont icon-jinru"></view>
+    </view>
+    <!-- 第三方登录 -->
+    <view class="other-login-type flex-c-c">第三方登录</view>
+    <home-other-login></home-other-login>
+    
+    <!-- 协议 -->
+    <view class="login-ruler flex-c-c">
+      注册及代表您同意<view class="agreement">《仿糗事百科协议》</view>
+    </view>
 	</view>
 </template>
 
 <script>
   import uniStatusBar from "../../components/uni-status-bar/uni-status-bar.vue"
+  import HomeOtherLogin from '../../components/home/HomeOtherLogin.vue'
 	export default {
     components:{
-      uniStatusBar
+      uniStatusBar,
+      HomeOtherLogin
     },
 		data() {
 			return {
-				
+				disabled:false,
+				loading: false
 			}
 		},
 		methods: {
-			
+      // 返回上一层及
+			onBack(){
+        
+      }
 		}
 	}
 </script>
 
-<style>
-
+<style lang="stylus">
+@import "../../common/css/form.css"
+// 忘记密码
+.login-password-wrapper
+  position: relative;
+  .phone
+    position: absolute;
+    left: 10upx;
+    top: 25%;
+  .phone-input
+    padding-left: 100upx;
+  .forgot-input
+    padding-right: 150upx;
+  .forgot-password
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    right: 0;
+    top: 0;
+    width: 150upx;
+    height: 100%;
+    z-index 10
+  // 验证码
+  .get-yzm
+    position: absolute;    
+    right: 0;
+    top: 15upx;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #eee;
+    border-radius 10upx
+    font-size: 25upx;
+    height: 50%;
+    
+    width: 150upx;
+    padding: 10upx 0;
+.other-login-type
+  position: relative;
+  color #CCCCCC
+  &::after,
+  &::before
+    content ""
+    position: absolute;
+    background-color: #BBBBBB;
+    height: 1upx;
+    width: 100upx;
+    top: 50%;
+  &::after
+    left: 25%;
+  &::before
+    right: 25%;
+.flex-c-c
+  padding: 20upx 0;
+.login-head
+  width: 100vw;
+.icon-guanbi
+  position: fixed;
+  top: 60upx;
+  left: 30upx;
+  font-size: 40upx;
+  font-weight: 700;
+  color #332f0a;
+  z-index 9
+.agreement    
+  color #007AFF
 </style>
